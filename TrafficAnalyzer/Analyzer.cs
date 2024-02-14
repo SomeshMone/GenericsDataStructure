5e3eeeec17dcefcfd7bba63bd6772417d4b71dbf
unknown <somesh.mone06@gmail.com>
2024-02-14 14:43:10 +0530

    commit

---
+++ TrafficAnalyzer/Analyzer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace TrafficAnalyzer
+{
+    public class Analyzer
+    {
+        public static List<string> CollectionData(string filepath)
+        {
+            List<string> rawData = new List<string>();
+            using (StreamReader reader = new StreamReader(filepath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    rawData.Add(line);
+                }
+            }
+            return rawData;
+        }
+    }
+}
---
+++ TrafficAnalyzer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using TrafficAnalyzer;
+
+namespace TrafficAnalyzer
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\somesh\OneDrive\Desktop\statistics.txt";
+
+
+            if (File.Exists(filepath))
+            {
+                try
+                {
+                    //Acceesing data from file adding into the list
+                    List<string> rawData = Analyzer.CollectionData(filepath);
+
+
+                    //perform Analysis
+
+                    Dictionary<string, int> statistics = new Dictionary<string, int>();
+
+                    foreach (string Ip in rawData)
+                    {
+                        // let's assume data entries are IP addresses
+                        if (statistics.ContainsKey(Ip))
+                        {
+                            statistics[Ip]++;
+                        }
+                        else
+                        {
+                            statistics[Ip] = 1;
+                        }
+                    }
+
+                    // Display 
+
+                    Console.WriteLine("Analysis Results:");
+
+                    foreach (var entry in statistics)
+                    {
+                        Console.WriteLine($"{entry.Key}: {entry.Value} occurrences");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("File is not Existed");
+
+            }
+        }
+    }
+}
+
